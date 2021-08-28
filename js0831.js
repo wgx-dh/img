c923db6c54af2163d40e4f7c59775bd40623207e
wgx-dh <63238251+wgx-dh@users.noreply.github.com>
2021-08-29 00:59:57 +0800

    Update and rename js0830.js to js0831.js

--- js0831.js
+++ js0831.js
@@ -1,5 +1,4 @@
-<script>
-  function getDays(){
+function getDays(){
     var date1 = '2020/12/13 17:00:00';
     var date2 = new Date();
     var date3 = date2.getTime() - new Date(date1).getTime();
@@ -7,6 +6,5 @@
     var days = Math.floor(date3/(24*3600*1000));
     retuen days
   }
+document.getElementById('love_time').innerHTML=getDays();
 
-    document.getElementById('love_time').innerHTML=getDays();
-</script>
